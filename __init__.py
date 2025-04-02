70a4cc917b9ef12efbc6ff8a54a1ea19cbc6fe0f
Dihan <dihan.algama@gmail.com>
2025-04-02 15:26:32 +0100

    Add FaceLineMask node to __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
 from .face_detection_marker import FaceDetectionMarker
+from .face_line_mask import FaceLineMask
 
 NODE_CLASS_MAPPINGS = {
-    "FaceDetectionMarker": FaceDetectionMarker
+    "FaceDetectionMarker": FaceDetectionMarker,
+    "FaceLineMask": FaceLineMask
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "FaceDetectionMarker": "Face Detection Marker"
+    "FaceDetectionMarker": "Face Detection Marker",
+    "FaceLineMask": "Face Line Mask"
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS'] 
